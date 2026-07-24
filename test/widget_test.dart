3a68e9ebcf0c14a42301b99cc8862b4b7f1bb2ca
Simone Massenzio <simone.massenzio@proton.me>
2026-07-24 13:45:54 +0800

    Fix widget_test.dart formatting and imports

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,12 +1,9 @@
-import 'package:flutter_test/flutter_test.dart';
 import 'package:app_patente_taiwanese/main.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('App launches smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
-    
-    // Just verify the app builds without crashing.
     expect(find.byType(MyApp), findsOneWidget);
   });
 }
